562220f22b048362aba53cef86bfe1a89c015aa4
lucas <lcspatsy@gmail.com>
2025-06-06 15:29:55 +0700

    removal of the config, reuirements, and credentials

--- config.py
+++
@@ -1,3 +0,0 @@
-SHEET_URL = "https://docs.google.com/spreadsheets/d/1fYw2-XY_gmhA1Ybhb4ZpojpHHjuLUUH-rwjrurhh0T0/edit?gid=1383858238#gid=1383858238"
-SHEET_NAME = "Copy of PR Singha Galleryc -1"
-CREDENTIALS_PATH = "credentials/service_account.json"
